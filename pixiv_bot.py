b38e2145dc1ebc8d021bdc7348445036a3679241
castaneai <castaneai@castaneai.net>
2015-12-20 16:21:13 +0900

    update

--- pixiv_bot.py
+++ pixiv_bot.py
@@ -1,6 +1,36 @@
 # -*- coding: utf-8 -*-
+import os
 from slacker import Slacker
 from pixivpy3 import PixivAPI
+
+PIXIV_COLOR_HEX = "#4385B7"
+
+
+def pixiv_get_following_works(pixiv: PixivAPI, start_timestamp=None):
+    page = 1
+    while True:
+        result = pixiv.me_following_works(page=page)
+        if "response" not in result:
+            break
+        page += 1
+        yield from result["response"]
+
+
+def pixiv_work_filter(work):
+    return work["age_limit"] != ["r18"]
+
+
+def pixiv_work_to_slack_attachment(work):
+    return {
+        "fallback": "[pixiv]New following user works.",
+        "color": PIXIV_COLOR_HEX,
+        "title": work["title"],
+        "title_link": "http://www.pixiv.net/member_illust.php?mode=medium&illust_id={}".format(work["id"]),
+        "image_url": work["image_urls"]["px_128x128"],
+        "author_name": work["user"]["name"],
+        "author_icon": work["user"]["profile_image_urls"]["px_50x50"],
+        "text": work["caption"],
+    }
 
 
 def post_to_slack(channel, work_attachment):
@@ -8,18 +38,9 @@ def post_to_slack(channel, work_attachment):
     slack.chat.post_message(channel, "", as_user=True, attachments=[work_attachment])
 
 if __name__ == "__main__":
+    # slack = Slacker(os.environ["PIXIV_BOT_SLACK_API_TOKEN"])
     pixiv = PixivAPI()
-    pixiv.login("<username>", "<password>")
-    r = pixiv.me_following_works()
-    for work in [w for w in r["response"] if w["age_limit"] != "r18"]:
-        attachment = {
-            "fallback": "pixiv new following works",
-            "color": "good",
-            "title": work["title"],
-            "title_link": "http://www.pixiv.net/member_illust.php?mode=medium&illust_id={}".format(work["id"]),
-            "image_url": work["image_urls"]["px_128x128"],
-            "author_name": work["user"]["name"],
-            "author_icon": work["user"]["profile_image_urls"]["px_50x50"],
-            "text": work["caption"],
-        }
-        post_to_slack("#pixiv_test", attachment)
+    pixiv.login(os.environ["PIXIV_BOT_USERNAME"], os.environ["PIXIV_BOT_PASSWORD"])
+
+    w = next(pixiv_get_following_works(pixiv))
+    print(w)
